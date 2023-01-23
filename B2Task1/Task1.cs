c06c7e7e67c65442eaecace816617af498de10d3
David MacKinnon <davidkwpg@gmail.com>
2023-01-22 22:30:58 -0600

    add error handling and pop ups with details

--- B2Task1/Task1.cs
+++ B2Task1/Task1.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using System.Windows.Forms;
 
 namespace B2Task1
 {
@@ -14,43 +15,62 @@ namespace B2Task1
 
         private void SaveFile_Click(object sender, EventArgs e)
         {
-            Stream myStream;
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "xml Files (*.xml)|*.*";
-            saveFileDialog.Title = "Save XML File";
-
-            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            try
             {
-                if ((myStream = saveFileDialog.OpenFile()) != null)
+                Stream myStream;
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "xml Files (*.xml)|*.*";
+                saveFileDialog.Title = "Save XML File";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    System.Xml.XmlTextWriter xmlWriter = new System.Xml.XmlTextWriter(myStream, System.Text.Encoding.Unicode);
-                    dataSet.WriteXml(xmlWriter);
-                    myStream.Close();
-
-
-                    //if you don't include the extension its saved without one
-                    //may be a better way to do this, but this works
-                    if (!saveFileDialog.FileName.EndsWith(".xml"))
+                    if ((myStream = saveFileDialog.OpenFile()) != null)
                     {
-                        File.Move(saveFileDialog.FileName, saveFileDialog.FileName + ".xml");
-                    }                                        
+                        System.Xml.XmlTextWriter xmlWriter = new System.Xml.XmlTextWriter(myStream, System.Text.Encoding.Unicode);
+                        dataSet.WriteXml(xmlWriter);
+                        myStream.Close();
+
+
+                        //if you don't include the extension its saved without one
+                        //may be a better way to do this, but this works
+                        if (!saveFileDialog.FileName.EndsWith(".xml"))
+                        {
+                            File.Move(saveFileDialog.FileName, saveFileDialog.FileName + ".xml");
+                        }
+                    }
                 }
             }
+            catch(Exception ex)
+            {
+                ShowError("Error saving File, please retry", ex);
+            }            
         }
 
         private void LoadNewFile_Click(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = null;
-            dataGridView1.Columns.Clear();
-            dataSet.Clear();
-            OpenFileDialog loadXmlFile = new OpenFileDialog();
-            loadXmlFile.Filter = "xml Files (*.xml)|*.*";
-
-            if (loadXmlFile.ShowDialog() == DialogResult.OK)
+            try
             {
-                dataSet.ReadXml(loadXmlFile.FileName);
-                dataGridView1.DataSource = dataSet.Tables[0];
+                dataGridView1.DataSource = null;
+                dataGridView1.Columns.Clear();
+                dataSet.Clear();
+                OpenFileDialog loadXmlFile = new OpenFileDialog();
+                loadXmlFile.Filter = "xml Files (*.xml)|*.*";
+
+                if (loadXmlFile.ShowDialog() == DialogResult.OK)
+                {
+                    dataSet.ReadXml(loadXmlFile.FileName);
+                    dataGridView1.DataSource = dataSet.Tables[0];
+                }
             }
+            catch(Exception ex)
+            {
+                ShowError("Error while loading File, please retry or check file to ensure it is valid",ex);
+            } 
+        }
+
+        private void ShowError(string message,Exception ex)
+        {
+            MessageBox.Show(message + "\n\n" + ex.ToString(), "Error Loading File",  MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
